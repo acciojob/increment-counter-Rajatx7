099fdb817c26686a14c5cf1f5e69bbc3260181b4
Rajat Singh <155432801+Rajatx7@users.noreply.github.com>
2024-02-02 14:16:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 function inc(){
     let p = document.getElementById("counter");
-    alert(p.innerText);
+    alert(parseInt(p.innerText));
     p.innerText = parseInt(p.innerText) + 1;
 }
 

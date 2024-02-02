9ba0abe98702bbbc6de4f6d6d893876ae45156ef
Rajat Singh <155432801+Rajatx7@users.noreply.github.com>
2024-02-02 14:05:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function inc(){
+    let p = document.getElementById("counter");
+    alert(p.innerText);
+    p.innerText = parseInt(p.innerText) + 1;
+}
+
+let btn = document.getElementById("incrementBtn");
+btn.addEventListener("click",inc)
